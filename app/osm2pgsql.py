b48663cf231f2a5e98b1610c3ed1130584d87b9a
Raymond DeVries <raymond.l.devries@gmail.com>
2021-08-25 00:58:42 -0700

    Refactor function name

--- app/osm2pgsql.py
+++ app/osm2pgsql.py
@@ -1,10 +1,12 @@
+import os
 import subprocess
 
 
-def main(path_name):
-    result = subprocess.call([f"osm2pgsql {path_name} -H 0.0.0.0 -P 5433 -d postgres -U postgres"], shell=True)
-    print(result)
+def import_osm(path_name):
+    subprocess.call(
+        [f"osm2pgsql {path_name} -H 0.0.0.0 -P {os.getenv('POSTGRES_PORT', 5432)} -d postgres -U postgres"], shell=True
+    )
 
 
-if __name__ == '__main__':
-    main("map.osm")
+if __name__ == "__main__":
+    import_osm("osm_files/example.osm")
