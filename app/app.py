cb1ab998c500d20f4d9a96a08a2ce7269219fc36
Raymond DeVries <raymond.l.devries@gmail.com>
2021-08-25 01:02:48 -0700

    User upload functionality added

--- app/app.py
+++ app/app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+import os
+import pathlib
+
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>helllo, World</p>"
+def home():
+    print(os.listdir(pathlib.Path(__file__).parent / "osm_files"))
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        f = request.files["osm_file"]
+        filename = secure_filename(f.filename)
+        f.save(f"osm_files/{filename}")
+        return home()
