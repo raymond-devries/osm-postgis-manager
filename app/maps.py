d4891911e47701b87cb699c65a458ae445bfc3c8
Raymond DeVries <raymond.l.devries@gmail.com>
2021-08-25 00:58:09 -0700

    Setup qgis functionality.
    Due to weird quirks with applying beautiful osm style layers. Custom style layers will need to be created to allow for automated application of beautiful osm layers.

--- app/maps.py
+++ app/maps.py
@@ -1,4 +1,24 @@
+import os
+from collections import namedtuple
+
 from qgis.core import *
+
+OsmLayer = namedtuple("OsmLayer", ("name", "type", "geometry_column"))
+
+OSM_PREFIX = "planet_osm_"
+
+OSM_LAYERS = ("polygon", "roads", "line", "point")
+
+
+# Importing layers works fine however external styles cannot be implemented. Either custom styles need to be created
+# or this quirk needs a workaround
+def import_osm_layers(uri: QgsDataSourceUri):
+    for osm_layer_name in OSM_LAYERS:
+        name = OSM_PREFIX + osm_layer_name
+        uri.setDataSource("public", name, "way")
+        qgis_layer = QgsVectorLayer(uri.uri(False), name, "postgres")
+
+        QgsProject.instance().addMapLayer(qgis_layer)
 
 
 def main():
@@ -7,10 +27,10 @@ def main():
 
     uri = QgsDataSourceUri()
 
-    uri.setConnection("0.0.0.0", "5433", "postgres", "postgres", "qgis")
+    uri.setConnection("0.0.0.0", os.getenv("POSTGRES_PORT", "5432"), "postgres", "postgres", "qgis")
 
     qgs.exitQgis()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
